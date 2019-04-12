838e293f6d20d1c883162b3f43073f1e6472168c
Takashi Motoki <t.motoki01@gmail.com>
2019-04-13 00:53:56 +0900

    scss追加、フォルダ構成変更

---
+++ src/view/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../scss/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      aaa
+      </div>
+    );
+  }
+}
+
+export default App;
