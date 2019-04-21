ad28b2367b2e13ba282ade7dd89e840418c16a33
Takashi Motoki <t.motoki01@gmail.com>
2019-04-22 00:43:54 +0900

    メニュー更新。スクロール促進アイコン追加

--- src/view/components/Main/index.js
+++ src/view/components/Main/index.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Arrow from '../Arrow';
 import Work from '../Work';
 import './style.scss';
 import face from '../../../img/top/face.png';
 
-export default (props) => {
+class Menu extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
 
-  return (
-    <div className='main'>
+  render() {
+    return (
+      <div className='main'>
+      <Work />
+      <Arrow />
       <div id='about'>
+        <hr />
         ロゴもメニューも暫定。<br />
         <br />
         <img alt='kao' src={face} /><br />
         （とりあえず顔）<br />
         <br />
-        元木 高志と申します。<br />
-        Web系のエンジニアで、バックエンドからフロントエンドまで対応します。<br />
-        <a href='https://github.com/t-motoki'>github</a>、Qiitaやってます！<br />
-        <br />
         〜覚書〜<br />
         <br />
-        ここは触りだけ。デザインも他と分けるかも<br />
+        ここは触りだけ。これが本来先頭。デザインも他と分けるかも<br />
         なるべく情報を厳選していく<br />
         <br />
         レスポンシブ対応していく<br />
@@ -27,10 +32,14 @@ export default (props) => {
         メニューはハンバーガーボタンを配置し、<br />
         ドロワーメニューにする<br />
         <br />
+        元木 高志と申します。<br />
+        Web系のエンジニアで、バックエンドからフロントエンドまで対応します。<br />
+        <a href='https://github.com/t-motoki'>github</a>、Qiitaやってます！<br />
+        <br />
         <br />
         <br />
       </div>
-      <Work />
+      <Arrow />
       スクロールテスト<br />
       <br />
       <br />
@@ -41,6 +50,7 @@ export default (props) => {
       <br />
       <br />
       <div id='history'>
+      <hr />
         この辺りまでスクロールされるはず<br />
         仕事の履歴を表で作る<br />
         なるべくシンプルに作る<br />
@@ -92,5 +102,8 @@ export default (props) => {
         <br />
       </div>
     </div>
-  );
-};
+    );
+  }
+}
+
+export default Menu;
--- src/view/components/Menu/index.js
+++ src/view/components/Menu/index.js
@@ -6,10 +6,10 @@ export default (props) => {
   const offset = 120;
   return (
     <div className='menu'>
-      <p className='logo' onClick={()=>alert('test')}>tmotoki.net</p>
+      <p className='logo' onClick={()=>alert('未実装')}>tmotoki.net</p>
       <div className='items'>
-        <AnchorLink className='item' href='#about' offset={offset}>About</AnchorLink>
         <AnchorLink className='item' href='#work' offset={offset}>Works</AnchorLink>
+        <AnchorLink className='item' href='#about' offset={offset}>About</AnchorLink>
         <AnchorLink className='item' href='#history' offset={offset}>History</AnchorLink>
       </div>
     </div>
